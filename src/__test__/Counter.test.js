970947f02f3c54405f1d81cf4f5836651c660337
sabrina palm <sabrina_palm@hotmail.com>
2018-06-17 13:49:33 +0200

    report

--- src/__test__/Counter.test.js
+++ src/__test__/Counter.test.js
@@ -2,31 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Counter from '../components/Counter.js';
 
-// shallow(<App/>)	- grund/ytlig rendering
-// mount(<App/>)		- full rendering, livscykel och DOM
-// render(<App/>)		- statisk renderin
-
-// shallow(jsxReactComponent)	-- shallow render, ShallowWrapper
-// find(selector)			returnera noder som matcher CSS selektorer
-// contains(jsxNode)		true om wrappern innehåller jsx-noden
-// hasClass(className)	noden har en CSS-klass
-// is(selector)			matchar en CSS-selektor
-// html()					gör om wrappern till en sträng med HTML
-// state(key)				returnerar värdet på komponentens state
-// setState(nextState)	sätter state
-// simulate(eventName)	simulera ett event på noden
-// render()				deep render
-// instance()				returnerar React-komponenten
-
-
-//Testa att den renderar korrekt.
-//Testa lägga till och ta bort
-//Testa att state uppdaterar
-
-
-
 describe('Counter renders without crashing', () => {
 
+  //component tests
   it('should render component without crashing', () => {
     let wrapper = shallow(<Counter />)
   })
@@ -43,12 +21,12 @@ describe('Counter renders without crashing', () => {
    expect(wrapper.find('.decrease').length).toBe(1)
  })
 
-
   it('should render input', () => {
     let wrapper = shallow(<Counter />)
     expect(wrapper.find('input').length).toBe(1)
   })
 
+  //functionality tests
   it('should test initialstate-value of Counter', ()=>{
     const wrapper =  shallow(<Counter />);
     wrapper.state('value');
--- src/__test__/List.test.js
+++ src/__test__/List.test.js
@@ -2,31 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import List from '../components/List.js';
 
-// shallow(<App/>)	- grund/ytlig rendering
-// mount(<App/>)		- full rendering, livscykel och DOM
-// render(<App/>)		- statisk renderin
-
-// shallow(jsxReactComponent)	-- shallow render, ShallowWrapper
-// find(selector)			returnera noder som matcher CSS selektorer
-// contains(jsxNode)		true om wrappern innehåller jsx-noden
-// hasClass(className)	noden har en CSS-klass
-// is(selector)			matchar en CSS-selektor
-// html()					gör om wrappern till en sträng med HTML
-// state(key)				returnerar värdet på komponentens state
-// setState(nextState)	sätter state
-// simulate(eventName)	simulera ett event på noden
-// render()				deep render
-// instance()				returnerar React-komponenten
-
-
-//Testa att den renderar korrekt.
-//Testa lägga till och ta bort
-//Testa att state uppdaterar
-
-
-
 describe('List renders without crashing', () => {
 
+  //component tests
   it('should render component without crashing', () => {
     const wrapper = shallow(<List />);
   })
@@ -53,6 +31,7 @@ describe('List renders without crashing', () => {
     expect(wrapper.contains(<ul className="list">{allAnimals}</ul>)).toBe(true)
   })
 
+  //functionality tests
   it('should test initialstate of my animalslist', ()=>{
     const wrapper =  shallow(<List />);
     wrapper.state('myAnimalsList');
